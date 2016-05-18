2e6b4fe2ab8104cc1253cc38aadd998f75b806a2
kylieJames <kaila@inbox.ru>
2016-05-18 17:44:28 +0300

    Adding homework 3 modified 4

--- lesson3/exercise4.py
+++ lesson3/exercise4.py
@@ -3,31 +3,39 @@
 
 import csv
 
-
 data = []
-with open('Python for QA - bugs list - Sheet1.csv', 'r') as the_file:
-    reader = csv.DictReader(the_file)
+
+
+def substitution(file, param):
+    reader = csv.DictReader(file)
+
     for row in reader:
         try:
-            if row['Priority'] == 'critical':
-                row['Priority'] = 'high'
+            if row[param] == 'critical':
+                row[param] = 'high'
                 data.append(row)
-            elif row['Priority'] == 'high':
-                row['Priority'] = 'medium'
+            elif row[param] == 'high':
+                row[param] = 'medium'
                 data.append(row)
-            elif row['Priority'] == 'medium':
-                row['Priority'] = 'low'
+            elif row[param] == 'medium':
+                row[param] = 'low'
                 data.append(row)
         except IndexError as e:
             print(e)
             pass
 
-    with open('new.csv', 'w') as to_file:
-        header = ['#', 'Description', 'Environment', 'Priority', 'Owner', 'Created At']
-        writer = csv.DictWriter(to_file, fieldnames=header)
 
-        writer.writeheader()
-        writer.writerows(data)
+def rewrite(input_file, output_file):
+
+    with open(input_file, 'r') as the_file:
+        substitution(the_file, 'Priority')
+
+        with open(output_file, 'w') as to_file:
+            header = ['#', 'Description', 'Environment', 'Priority', 'Owner', 'Created At']
+            writer = csv.DictWriter(to_file, fieldnames=header)
+
+            writer.writeheader()
+            writer.writerows(data)
 
 
 def rows_amount(filename):
@@ -35,6 +43,8 @@ def rows_amount(filename):
         for i, line in enumerate(f, 1):
             pass
     print(i)
+
+rewrite('Python for QA - bugs list - Sheet1.csv', 'new.csv')
 
 rows_amount('new.csv')
 rows_amount('Python for QA - bugs list - Sheet1.csv')
